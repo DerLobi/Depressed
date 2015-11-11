57ae4d717c330bd2f4a486811b927da260254641
Christian Lobach <Christian.Lobach@gmail.com>
2015-11-11 21:08:35 +0100

    set up navbar title appearance

--- Depressed?/AppDelegate.swift
+++ Depressed?/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
         window?.tintColor = UIColor(red:0.44, green:0.24, blue:0.78, alpha:1)
+        UINavigationBar.appearance().titleTextAttributes = [
+            NSForegroundColorAttributeName: UIColor.whiteColor()
+        ]
+        
         return true
     }
 
