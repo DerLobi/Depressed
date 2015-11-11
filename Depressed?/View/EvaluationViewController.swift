91bea94be7a8d899a662773f13a411e3112cd649
Christian Lobach <Christian.Lobach@gmail.com>
2015-11-12 00:07:20 +0100

    update labels with viewmodel data

--- Depressed?/View/EvaluationViewController.swift
+++ Depressed?/View/EvaluationViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class EvaluationViewController: UIViewController {
     
-    var viewModel: EvaluationViewModel?
+    var viewModel: EvaluationViewModel? {
+        didSet {
+            if let viewModel = viewModel {
+                updateLabelsWithViewModel(viewModel)
+            }
+        }
+    }
     
     @IBOutlet weak var resultLabel: UILabel!
     
@@ -19,6 +25,23 @@ class EvaluationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if let viewModel = viewModel {
+            updateLabelsWithViewModel(viewModel)
+        }
     }
 
+    private func updateLabelsWithViewModel(viewModel: EvaluationViewModel) {
+        guard isViewLoaded() else { return }
+        
+        let diagnosisText = viewModel.diagnosisText as NSString
+        
+        let attributedDiagnosisText = NSMutableAttributedString(string: viewModel.diagnosisText)
+        let diagnosisRange = diagnosisText.rangeOfString(viewModel.diagnosis)
+
+        attributedDiagnosisText.addAttribute(NSFontAttributeName, value: UIFont.boldSystemFontOfSize(17.0), range: diagnosisRange)
+        
+        resultLabel.attributedText = attributedDiagnosisText
+        additionalLabel.text = viewModel.suicidalText
+    }
+    
 }
