952076a003d8b769b272b879beeb322d588a562e
Christian Lobach <Christian.Lobach@gmail.com>
2015-11-26 18:27:26 +0100

    Add UITests for `snapshot`

---
+++ Depressed?UITests/Depressed_UITests.swift
@@ -0,0 +1,87 @@
+//
+//  Depressed_UITests.swift
+//  Depressed?UITests
+//
+//  Created by Christian Lobach on 26/11/15.
+//  Copyright © 2015 Christian Lobach. All rights reserved.
+//
+
+import XCTest
+
+class Depressed_UITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+
+        let app = XCUIApplication()
+        setLanguage(app)
+        app.launch()
+        
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testScreenshots() {
+
+        let app = XCUIApplication()
+        snapshot("01Start")
+        
+        var startButton: XCUIElement!
+        var nearlyEveryDayCell: XCUIElement!
+        var severalDaysCell: XCUIElement!
+        var moreThanHalfTheDaysCell: XCUIElement!
+        var notAtAllCell: XCUIElement!
+        
+        if deviceLanguage == "de" {
+            startButton = app.buttons["Test starten"]
+            nearlyEveryDayCell = app.tables.staticTexts["Beinahe jeden Tag"]
+            severalDaysCell = app.tables.staticTexts["An einzelnen Tagen"]
+            moreThanHalfTheDaysCell = app.tables.staticTexts["An mehr als der Hälfte der Tage"]
+            notAtAllCell = app.tables.staticTexts["Überhaupt nicht"]
+        } else {
+            startButton = app.buttons["Start the Test"]
+            nearlyEveryDayCell = app.tables.staticTexts["Nearly every day"]
+            severalDaysCell = app.tables.staticTexts["Several days"]
+            moreThanHalfTheDaysCell = app.tables.staticTexts["More than half the days"]
+            notAtAllCell = app.tables.staticTexts["Not at all"]
+        }
+        
+        startButton?.tap()
+
+        sleep(1)
+        snapshot("02FirstQuestion")
+
+        nearlyEveryDayCell.tap()
+
+        sleep(1)
+        severalDaysCell.tap()
+
+        sleep(1)
+        moreThanHalfTheDaysCell.tap()
+
+        sleep(1)
+        severalDaysCell.tap()
+
+        sleep(1)
+        moreThanHalfTheDaysCell.tap()
+
+        sleep(1)
+        moreThanHalfTheDaysCell.tap()
+
+        sleep(1)
+        severalDaysCell.tap()
+        
+        sleep(1)
+        notAtAllCell.tap()
+
+        sleep(1)
+        snapshot("03LastQuestion")
+        notAtAllCell.tap()
+                
+        snapshot("04Result")
+    }
+    
+}
---
+++ Snapfile
@@ -0,0 +1,17 @@
+devices([
+  "iPhone 6",
+  "iPhone 6 Plus",
+  "iPhone 5",
+  "iPhone 4s"
+  ])
+
+languages([
+  "en-US",
+  "de-DE"
+])
+
+scheme "Depressed?UITests"
+
+output_directory "./screenshots"
+
+clear_previous_screenshots
