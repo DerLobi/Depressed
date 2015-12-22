5a324de190a1d34125df6f09f9c2f5cd14ed0601
Christian Lobach <Christian.Lobach@gmail.com>
2015-12-22 22:36:32 +0100

    make steps a property

--- Depressed?/Model/SelfTestTask.swift
+++ Depressed?/Model/SelfTestTask.swift
@@ -3,52 +3,11 @@ import ResearchKit
 /// Task that contains the questions of the [PHQ-9](https://en.wikipedia.org/wiki/Patient_Health_Questionnaire#Versions) questionnaire.
 public class SelfTestTask: NSObject, ORKTask {
 
+    private let steps: [ORKStep]
+    
     public let identifier = "SelfTest"    
 
-    public func stepBeforeStep(step: ORKStep?, withResult result: ORKTaskResult) -> ORKStep? {
-        if let step = step {
-            // return appropiate step
-            let index = steps().indexOf(step)
-            if let index = index where index - 1 >= 0 {
-                return steps()[index - 1]
-            } else {
-                return nil
-            }
-        } else {
-            return nil
-            
-        }
-    }
-
-    public func stepAfterStep(step: ORKStep?, withResult result: ORKTaskResult) -> ORKStep? {
-
-        if let step = step {
-            // return appropiate step
-            let index = steps().indexOf(step)
-            if let index = index where index + 1 < steps().count {
-                return steps()[index + 1]
-            } else {
-                return nil
-            }
-        } else {
-            // return first step
-            return steps().first!
-        }
-
-    }
-    
-    public func progressOfCurrentStep(step: ORKStep, withResult result: ORKTaskResult) -> ORKTaskProgress {
-        let index = steps().indexOf(step)
-
-        if let index = index {
-            return ORKTaskProgressMake(UInt(index), UInt(steps().count))
-        }
-        
-        return ORKTaskProgressMake(0, UInt(steps().count))
-    }
-    
-    private func steps() -> [ORKStep] {
-
+    public override init() {
         let pleasureInterestQuestion = Question(identifier: .LosingInterest)
         let depressedQuestion = Question(identifier: .FeelingDepressed)
         let sleepQuestion = Question(identifier: .TroubleSleeping)
@@ -59,7 +18,7 @@ public class SelfTestTask: NSObject, ORKTask {
         let slowFastQuestion = Question(identifier: .SlowOrFast)
         let suicideQuestion = Question(identifier: .FeelingSuicidal)
         
-        return [
+        steps = [
             pleasureInterestQuestion.step,
             depressedQuestion.step,
             sleepQuestion.step,
@@ -70,6 +29,47 @@ public class SelfTestTask: NSObject, ORKTask {
             slowFastQuestion.step,
             suicideQuestion.step
         ]
+    }
+
+    public func stepBeforeStep(step: ORKStep?, withResult result: ORKTaskResult) -> ORKStep? {
+        if let step = step {
+            // return appropiate step
+            let index = steps.indexOf(step)
+            if let index = index where index - 1 >= 0 {
+                return steps[index - 1]
+            } else {
+                return nil
+            }
+        } else {
+            return nil
+        }
+    }
+
+    public func stepAfterStep(step: ORKStep?, withResult result: ORKTaskResult) -> ORKStep? {
+
+        if let step = step {
+            // return appropiate step
+            let index = steps.indexOf(step)
+            if let index = index where index + 1 < steps.count {
+                return steps[index + 1]
+            } else {
+                return nil
+            }
+        } else {
+            // return first step
+            return steps.first!
+        }
+
+    }
+    
+    public func progressOfCurrentStep(step: ORKStep, withResult result: ORKTaskResult) -> ORKTaskProgress {
+        let index = steps.indexOf(step)
+
+        if let index = index {
+            return ORKTaskProgressMake(UInt(index), UInt(steps.count))
+        }
+        
+        return ORKTaskProgressMake(0, UInt(steps.count))
     }
 
 }
