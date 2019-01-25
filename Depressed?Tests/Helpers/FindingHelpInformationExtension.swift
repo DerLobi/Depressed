a9c43436b5075f088ac066eb03a67d54f9af5505
Christian Lobach <Christian.Lobach@gmail.com>
2019-01-25 23:54:03 +0100

    implement initializer in module to prevent warning

--- Depressed?Tests/Helpers/FindingHelpInformationExtension.swift
+++
@@ -1,9 +0,0 @@
-import Depressed
-import Foundation
-
-extension FindingHelpInformation {
-    init(url: URL, organizationName: String) {
-        self.url = url
-        self.organizationName = organizationName
-    }
-}
