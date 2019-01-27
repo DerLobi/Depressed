86ac265261af9f4ece5d406a49e84739cb2cd522
Christian Lobach <Christian.Lobach@gmail.com>
2019-01-27 17:55:26 +0100

    fix compiling for Interface Builder

--- Depressed?/View/Appearance.swift
+++ Depressed?/View/Appearance.swift
@@ -10,8 +10,9 @@ import Foundation
 import UIKit
 
 struct Appearance {
-    static let endeavour = UIColor(named: "Endeavour")!
-    static let elephant = UIColor(named: "Elephant")!
+    
+    static let endeavour = UIColor(named: "Endeavour", in: Bundle(for: AppDelegate.self), compatibleWith: nil)!
+    static let elephant = UIColor(named: "Elephant", in: Bundle(for: AppDelegate.self), compatibleWith: nil)!
 
     static func setUp() {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
